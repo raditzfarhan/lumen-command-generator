60e16b351446ec9e3db7eb2d5cfa91534a203113
Farhan <ahmad.farhan@globaltium.com>
2020-04-13 23:01:02 +0800

    Add Service Provider
    Add generate:model command

---
+++ src/Commands/ModelGenerateCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace RaditzFarhan\LumenCommandGenerator\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+
+class ModelGenerateCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'generate:model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Eloquent model class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Model';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (parent::handle() === false && !$this->option('force')) {
+            return false;
+        }
+
+        if ($this->option('all')) {
+            $this->input->setOption('factory', true);
+        }
+
+        if ($this->option('migration')) {
+            $this->createMigration();
+        }
+    }
+
+    /**
+     * Create a migration file for the model.
+     *
+     * @return void
+     */
+    protected function createMigration()
+    {
+        $table = Str::snake(Str::pluralStudly(class_basename($this->argument('name'))));
+
+        if ($this->option('pivot')) {
+            $table = Str::singular($table);
+        }
+
+        $this->call('make:migration', [
+            'name' => "create_{$table}_table",
+            '--create' => $table,
+        ]);
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('pivot')) {
+            return __DIR__ . '/stubs/pivot.model.stub';
+        }
+
+        return __DIR__ . '/stubs/model.stub';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['all', 'a', InputOption::VALUE_NONE, 'Generate a migration for the model as well'],
+
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the model already exists'],
+
+            ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model'],
+
+            ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model'],
+        ];
+    }
+}
